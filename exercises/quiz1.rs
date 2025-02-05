91c45d82501347def0bf4a71fc1b6c67e56aee67
huanglin_bupt <alin.sdr@gmail.com>
2025-02-05 16:56:04 +0800

    update

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples (num_apple: u64) -> u64 {
+    if num_apple > 40 {
+        num_apple*1
+    } else {
+        num_apple*2
+    }
+}
 
 // Don't modify this function!
 #[test]
